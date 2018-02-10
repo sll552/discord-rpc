147c578fbee89b72e9a33ee8313437c6b432b48d
Stefan Lengauer <slengauer522@gmail.com>
2018-02-10 17:47:47 +0100

    - add example for visual c#

---
+++ examples/sharp-presence/sharp-presence/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace sharp_presence
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBoxState = new System.Windows.Forms.TextBox();
+      this.textBoxDetails = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBoxPartyId = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.textBoxPartyMax = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.textBoxPartySize = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.textBoxLargeImageKey = new System.Windows.Forms.TextBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBoxLargeImageText = new System.Windows.Forms.TextBox();
+      this.label7 = new System.Windows.Forms.Label();
+      this.textBoxSmallImageKey = new System.Windows.Forms.TextBox();
+      this.label8 = new System.Windows.Forms.Label();
+      this.textBoxSmallImageText = new System.Windows.Forms.TextBox();
+      this.label9 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(13, 13);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(32, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "State";
+      // 
+      // textBoxState
+      // 
+      this.textBoxState.Location = new System.Drawing.Point(100, 10);
+      this.textBoxState.Name = "textBoxState";
+      this.textBoxState.Size = new System.Drawing.Size(133, 20);
+      this.textBoxState.TabIndex = 1;
+      // 
+      // textBoxDetails
+      // 
+      this.textBoxDetails.Location = new System.Drawing.Point(100, 36);
+      this.textBoxDetails.Name = "textBoxDetails";
+      this.textBoxDetails.Size = new System.Drawing.Size(133, 20);
+      this.textBoxDetails.TabIndex = 3;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(13, 39);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(39, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Details";
+      // 
+      // textBoxPartyId
+      // 
+      this.textBoxPartyId.Location = new System.Drawing.Point(100, 62);
+      this.textBoxPartyId.Name = "textBoxPartyId";
+      this.textBoxPartyId.Size = new System.Drawing.Size(133, 20);
+      this.textBoxPartyId.TabIndex = 5;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(13, 65);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(42, 13);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "PartyID";
+      // 
+      // textBoxPartyMax
+      // 
+      this.textBoxPartyMax.Location = new System.Drawing.Point(100, 88);
+      this.textBoxPartyMax.Name = "textBoxPartyMax";
+      this.textBoxPartyMax.Size = new System.Drawing.Size(133, 20);
+      this.textBoxPartyMax.TabIndex = 7;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(13, 91);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(51, 13);
+      this.label4.TabIndex = 6;
+      this.label4.Text = "PartyMax";
+      // 
+      // textBoxPartySize
+      // 
+      this.textBoxPartySize.Location = new System.Drawing.Point(100, 114);
+      this.textBoxPartySize.Name = "textBoxPartySize";
+      this.textBoxPartySize.Size = new System.Drawing.Size(133, 20);
+      this.textBoxPartySize.TabIndex = 9;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(13, 117);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(51, 13);
+      this.label5.TabIndex = 8;
+      this.label5.Text = "PartySize";
+      // 
+      // textBoxLargeImageKey
+      // 
+      this.textBoxLargeImageKey.Location = new System.Drawing.Point(100, 140);
+      this.textBoxLargeImageKey.Name = "textBoxLargeImageKey";
+      this.textBoxLargeImageKey.Size = new System.Drawing.Size(133, 20);
+      this.textBoxLargeImageKey.TabIndex = 11;
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(13, 143);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(81, 13);
+      this.label6.TabIndex = 10;
+      this.label6.Text = "LargeImageKey";
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.BackColor = System.Drawing.SystemColors.Control;
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.button1.Location = new System.Drawing.Point(12, 272);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(226, 35);
+      this.button1.TabIndex = 12;
+      this.button1.Text = "Update Presence";
+      this.button1.UseVisualStyleBackColor = false;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBoxLargeImageText
+      // 
+      this.textBoxLargeImageText.Location = new System.Drawing.Point(100, 166);
+      this.textBoxLargeImageText.Name = "textBoxLargeImageText";
+      this.textBoxLargeImageText.Size = new System.Drawing.Size(133, 20);
+      this.textBoxLargeImageText.TabIndex = 14;
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(13, 169);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(84, 13);
+      this.label7.TabIndex = 13;
+      this.label7.Text = "LargeImageText";
+      // 
+      // textBoxSmallImageKey
+      // 
+      this.textBoxSmallImageKey.Location = new System.Drawing.Point(100, 192);
+      this.textBoxSmallImageKey.Name = "textBoxSmallImageKey";
+      this.textBoxSmallImageKey.Size = new System.Drawing.Size(133, 20);
+      this.textBoxSmallImageKey.TabIndex = 16;
+      // 
+      // label8
+      // 
+      this.label8.AutoSize = true;
+      this.label8.Location = new System.Drawing.Point(13, 195);
+      this.label8.Name = "label8";
+      this.label8.Size = new System.Drawing.Size(79, 13);
+      this.label8.TabIndex = 15;
+      this.label8.Text = "SmallImageKey";
+      // 
+      // textBoxSmallImageText
+      // 
+      this.textBoxSmallImageText.Location = new System.Drawing.Point(100, 218);
+      this.textBoxSmallImageText.Name = "textBoxSmallImageText";
+      this.textBoxSmallImageText.Size = new System.Drawing.Size(133, 20);
+      this.textBoxSmallImageText.TabIndex = 18;
+      // 
+      // label9
+      // 
+      this.label9.AutoSize = true;
+      this.label9.Location = new System.Drawing.Point(13, 221);
+      this.label9.Name = "label9";
+      this.label9.Size = new System.Drawing.Size(82, 13);
+      this.label9.TabIndex = 17;
+      this.label9.Text = "SmallImageText";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(250, 319);
+      this.Controls.Add(this.textBoxSmallImageText);
+      this.Controls.Add(this.label9);
+      this.Controls.Add(this.textBoxSmallImageKey);
+      this.Controls.Add(this.label8);
+      this.Controls.Add(this.textBoxLargeImageText);
+      this.Controls.Add(this.label7);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBoxLargeImageKey);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.textBoxPartySize);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.textBoxPartyMax);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.textBoxPartyId);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.textBoxDetails);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.textBoxState);
+      this.Controls.Add(this.label1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Name = "Form1";
+      this.ShowIcon = false;
+      this.Text = "Sharp-Presence";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBoxState;
+    private System.Windows.Forms.TextBox textBoxDetails;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBoxPartyId;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox textBoxPartyMax;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox textBoxPartySize;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.TextBox textBoxLargeImageKey;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox textBoxLargeImageText;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.TextBox textBoxSmallImageKey;
+    private System.Windows.Forms.Label label8;
+    private System.Windows.Forms.TextBox textBoxSmallImageText;
+    private System.Windows.Forms.Label label9;
+  }
+}
+
---
+++ examples/sharp-presence/sharp-presence/README.md
@@ -0,0 +1,4 @@
+# Sharp-Presence Demo
+This demo solution opens a simple GUI and updates the discord rich presence when the button is clicked
+
+It includes a modified wrapper which is based on the Unity button clicker demo and extended to support UTF-8 under Visual C#
